8d0863c20279d9fecceaa2660432e9322b887458
LeonardoFlores1 <leonardoflores150@gmail.com>
2018-11-22 21:17:49 -0600

    actualizando

--- coneccion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$usuario="root";
-	$pass	="";
-	$servidor="localhost";
-	$nombre_base = "isw";
-	$coneccion=mysqli_connect($servidor,$usuario,$pass)or die('No se puede conectar al servidor');
-	mysqli_select_db($coneccion,$nombre_base);
-	mysqli_set_charset($coneccion, "utf8"); 
-?>
